59b7cfa2b8894294382f57e3a7323229f18cf12b
Maksim Rakitin <mrakitin@bnl.gov>
2022-04-25 03:16:53 -0400

    Download artifact from GH

--- test-upload.py
+++ test-upload.py
@@ -91,7 +91,7 @@ if __name__ == "__main__":
     counter = count(1)
     token = os.environ["ZENODO_SANDBOX_TOKEN"]
     resp = create_new_version(
-        conceptrecid="1054873", version="v0.0.4a9", token=token,
-        extra_files={"2022-2.0-py39-tiled.tar.gz": "rb"}
+        conceptrecid="1054873", version="v0.0.4a10", token=token,
+        extra_files={"2022-2.2-py39-tiled.tar.gz": "rb"}
     )
     pprint.pprint(resp)
