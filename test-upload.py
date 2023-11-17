519f01bacd4c746d4f1ad52d1302a2d71a9c4f2c
Max Rakitin <mrakitin@bnl.gov>
2023-11-17 02:22:46 -0500

    Upload 3.10 envs first

--- test-upload.py
+++ test-upload.py
@@ -273,13 +273,13 @@ if __name__ == "__main__":
             f"runner-{version}-py310.sh": "r",
             f"{version}-py310.tar.gz": "rb",
 
-            # Python 3.11 (non-tiled)
-            f"{version}-py311-md5sum.txt": "r",
-            f"{version}-py311-sha256sum.txt": "r",
-            f"{version}-py311.yml": "r",
-            f"Dockerfile-{version}-py311": "r",
-            f"runner-{version}-py311.sh": "r",
-            f"{version}-py311.tar.gz": "rb",
+            # # Python 3.11 (non-tiled)
+            # f"{version}-py311-md5sum.txt": "r",
+            # f"{version}-py311-sha256sum.txt": "r",
+            # f"{version}-py311.yml": "r",
+            # f"Dockerfile-{version}-py311": "r",
+            # f"runner-{version}-py311.sh": "r",
+            # f"{version}-py311.tar.gz": "rb",
 
             # # Python 3.8 (tiled)
             # f"{version}-py38-tiled-md5sum.txt": "r",
@@ -305,13 +305,13 @@ if __name__ == "__main__":
             f"runner-{version}-py310-tiled.sh": "r",
             f"{version}-py310-tiled.tar.gz": "rb",
 
-            # Python 3.11 (tiled)
-            f"{version}-py311-tiled-md5sum.txt": "r",
-            f"{version}-py311-tiled-sha256sum.txt": "r",
-            f"{version}-py311-tiled.yml": "r",
-            f"Dockerfile-{version}-py311-tiled": "r",
-            f"runner-{version}-py311-tiled.sh": "r",
-            f"{version}-py311-tiled.tar.gz": "rb",
+            # # Python 3.11 (tiled)
+            # f"{version}-py311-tiled-md5sum.txt": "r",
+            # f"{version}-py311-tiled-sha256sum.txt": "r",
+            # f"{version}-py311-tiled.yml": "r",
+            # f"Dockerfile-{version}-py311-tiled": "r",
+            # f"runner-{version}-py311-tiled.sh": "r",
+            # f"{version}-py311-tiled.tar.gz": "rb",
         },
     )
     pprint.pprint(resp)
